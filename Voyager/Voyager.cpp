629c1250d52542ed8eb6d7fe67bf4f6eb2f6bad7
BELLEVUE\mdietz <MDAllen@protonmail.com>
2021-09-11 14:53:48 -0700

    Class Add Test

--- Voyager/Voyager.cpp
+++ Voyager/Voyager.cpp
@@ -5,9 +5,18 @@
 
 #include "SDL_shape.h"
 
+class GameObject
+{
+public:
+
+private:
+
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
     SDL_Rect fillRect = { 1920 / 4, 1080 / 4, 1920 / 2, 1080 / 2 };
+
 }
 
