a24da4cb0dd616415784770f5bdbc2bb9c2f7abd
BELLEVUE\mdietz <MDAllen@protonmail.com>
2021-09-11 14:19:54 -0700

    Add project files.

---
+++ Voyager/Voyager.cpp
@@ -0,0 +1,13 @@
+// Voyager.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "SDL_shape.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    SDL_Rect fillRect = { 1920 / 4, 1080 / 4, 1920 / 2, 1080 / 2 };
+}
+
